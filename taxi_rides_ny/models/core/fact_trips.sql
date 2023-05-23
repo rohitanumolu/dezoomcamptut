98171f16bcf96f9af8ea4f91bead78e0bb497a0a
Rohit Anumolu <ranumolu1@student.gsu.edu>
2023-05-23 18:37:05 +0000

    Updated

--- taxi_rides_ny/models/core/fact_trips.sql
+++ taxi_rides_ny/models/core/fact_trips.sql
@@ -3,13 +3,13 @@
 with green_data as (
     select *, 
         'Green' as service_type 
-    from {{ ref('stg_green_tripdata') }}
+    from {{ ref('stg_green_trips') }}
 ), 
 
 yellow_data as (
     select *, 
         'Yellow' as service_type
-    from {{ ref('stg_yellow_tripdata') }}
+    from {{ ref('stg_yellow_trips') }}
 ), 
 
 trips_unioned as (
